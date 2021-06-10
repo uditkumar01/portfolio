eee973c1db43f8c60fdedc43c3c4b2d3081e795b
Udit Kumar <uditkumarussa@gmail.com>
2021-06-11 02:42:21 +0530

    fixed smooth scroll

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,14 @@ import ThemeProvider from "../context/ThemeProvider/ThemeProvider";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-    // useEffect(() => {
-    //     window.scroll({
-    //         top: 0,
-    //         left: 0,
-    //         behavior: "smooth",
-    //     });
-    // }, []);
+    useEffect(() => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+    }, []);
+
     return (
         <ThemeProvider>
             <Component {...pageProps} />
